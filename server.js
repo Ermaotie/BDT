10a0e4bbe2175fea733b5af48cf1ef2349b41928
Ermaotie <63703577+Ermaotie@users.noreply.github.com>
2021-05-24 22:36:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const port = process.env.PORT || 8443;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cors())
 
 
